50dda7bf4af1ac35318ee7a1d410ea4d747c0cc8
rr5510s <78934339+rrodney077@users.noreply.github.com>
2023-02-10 17:43:25 +0000

    t

--- FarmSim/Assets/Scripts/Movement.cs
+++ FarmSim/Assets/Scripts/Movement.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] GameObject camSwitcher;
     [SerializeField] private float speed = 5f;
+
+    private Vector2 movementInput = Vector2.zero;
     Vector3 forward;
     Vector3 right;
 
@@ -16,28 +19,33 @@ public class Movement : MonoBehaviour
         forward = Vector3.Normalize(forward);
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
     }
+
+    public void onMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
-        {
-            Move();            
-
-        }
+        Move();
     }
 
     void Move()
     {
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        Vector3 rightMov = right * speed * Time.deltaTime * Input.GetAxis("Horizontal");
-        Vector3 upMov = forward * speed * Time.deltaTime * Input.GetAxis("Vertical");
+        //Vector3 direction = new Vector3(movementInput.x, 0, movementInput.y);
+        Vector3 rightMov = right * speed * Time.deltaTime * movementInput.x;
+        Vector3 upMov = forward * speed * Time.deltaTime * movementInput.y;
 
         Vector3 heading = Vector3.Normalize(rightMov + upMov);
 
         transform.forward = heading;
 
-        transform.position += rightMov;
-        transform.position += upMov;
+        Vector3 movement = Vector3.Normalize(heading) * speed * Time.deltaTime;
+
+        //transform.position += rightMov;
+        //transform.position += upMov;
+
+        transform.position += movement;
         camSwitcher.GetComponent<CamSwitcher>().isMoveable = false;
 
     }
